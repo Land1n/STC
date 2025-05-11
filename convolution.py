d555ee6ca6c2e47c9a36a4852c76af5a97224c1a
Воробьев Антон <vorobevanton1285@gmail.com>
2025-05-11 16:12:59 +0300

    v1.0.1

--- convolution.py
+++ convolution.py
@@ -1,10 +1,10 @@
 import numpy as np
-"""
+
 from style_f import *
 from generationSignal import *
-"""
+
 #Свертка
-def convolution(firstSignal, SecondSignal):
+def convolution(firstSignal, SecondSignal, fs):
     svertka_func = []
     summ = 0
     SecondSignal = SecondSignal[::-1]
@@ -17,57 +17,68 @@ def convolution(firstSignal, SecondSignal):
         svertka_func.append(summ)
         summ = 0
     svertka_func = svertka_func/max(svertka_func)
-    return svertka_func
+    timeline = np.linspace(-1 * len(SecondSignal) / fs, (len(firstSignal)) / fs, fs * 2)
+    return timeline,svertka_func
 #Корреляция
-def correlation(firstSignal, SecondSignal):
+def correlation(firstSignal, SecondSignal,fs):
     correlation_func = []
     summ = 0
     for i in range (0,len(SecondSignal)):
         firstSignal = np.concatenate([[0],firstSignal,[0]])
+
     for j in range(0, len(firstSignal)-len(SecondSignal)):
         for i in range(0, len(SecondSignal)):
             summ += firstSignal[i + j] * SecondSignal[i]
         correlation_func.append(summ)
         summ = 0
     correlation_func = correlation_func/max(correlation_func)
-    return correlation_func
+    timeline = np.linspace(-1*len(SecondSignal)/fs, (len(firstSignal))/fs, fs*2)
+    return timeline, correlation_func
 #Пример:
 """
-t, pulse = rectangular_pulse(start=0,
-                             duration=100,
+t, pulse = rectangular_pulse(start=-0.5,
+                             duration=1,
                              amplitude=1,
-                             t_start=0,
-                             t_end=2,
-                             fs=2000)
+                             t_start=-1,
+                             t_end=1,
+                             fs=1000)
 
-#t, pulse = triangular_pulse(peak_time=0.5,t_start =0,t_end=2, A=1,fs=2000)
-t1, pulse1 =  gaussian_pulse(μ=1, σ=0.3, A=1, t_start=0, t_end=2, fs=2000)
+t1, pulse1 = rectangular_pulse(start=0,
+                             duration=1,
+                             amplitude=1,
+                             t_start=-1,
+                             t_end=1,
+                             fs=1000)
+#t, pulse1 = triangular_pulse(peak_time=0.5,t_start =0,t_end=2, A=1,fs=1000)
+#t1, pulse1 =  gaussian_pulse(μ=1, σ=0.3, A=1, t_start=0, t_end=2, fs=2000)
 
 #a=convolution(pulse,pulse1)
-#b=correlation(pulse,pulse1)
-
+tX,b=correlation(pulse,pulse1,1000)
+print(len(tX))
+print(len(b))
 pm = PlotManager()
 
 data_dict = {
-        'y1': pulse,
-        #'y2': b
+    'x':tX,
+        'y1':  b,
+        #'y2': pulse1
     }
 df = pm.load_data(data=data_dict)
 # Построение графиков с автоматическими точками по оси X
 pm.plot(
         data=df, #None, для скопированных данных
-        x_column=None,  # None, для автоматического расположения точек, 0 для скопированных данных
-        y_column=None,
-        labels={'y1': 'Сигнал', 'y2': 'График 2'},
-        title='Прямоугольный сигнал',
-        xlabel='Номер точки',
+        x_column='x',  # None, для автоматического расположения точек, 0 для скопированных данных
+        y_column='y1',
+        labels={'y1': 'Второй сигнал', 'y2': 'Второй сигнал'},
+        title='Сигналы используемые в корреляции',
+        xlabel='Время, с',
         ylabel='Значение',
         marker=None,
-        color='Blue',
+        color="saddlebrown",
         linewidth=1.5,
-        xstart=None,
+        xstart=-1,
         ystart=None,
-        xticks=None,
+        xticks=0.25,
         yticks=None,
 
         ask_input=False #True, если надо вводить скопированные столбцы
